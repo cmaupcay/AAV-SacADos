720a1cdc9ee7a0c323dda14c55563a7d388c6221
tensaiji <47110246+tensaiji@users.noreply.github.com>
2021-10-18 18:16:18 +0200

    fix PSE (sans borne sup.)

--- src/pse/PSE.java
+++ src/pse/PSE.java
@@ -17,16 +17,19 @@ public class PSE implements IAlgorithme
 	private Noeud _rechercher_solution(Noeud noeud, double poids_max)
 	{
 		this._noeuds_parcourus++;
-		if (noeud.borne_sup() < this._borne_inf) return noeud;
+		if (noeud.fils_bas == null) return noeud; // Le noeud est en fin de branche
 		
-		Noeud h = null;
-		if (noeud.fils_haut == null)
-			h = noeud;
-		else h = _rechercher_solution(noeud.fils_haut, poids_max);
-		if (noeud.fils_bas == null) return noeud;
-		
-		if (h.valeur() > noeud.valeur()) return h;
-		else return _rechercher_solution(noeud.fils_bas, poids_max);
+		Noeud b = _rechercher_solution(noeud.fils_bas, poids_max); // On cherche une solution dans la partie basse
+		if (noeud.fils_haut == null // La partie haute n'a pas été générée
+		 || noeud.fils_haut.borne_sup() < this._borne_inf) // La borne supérieure n'est pas intéressante
+			return b;
+		/*
+		if (noeud.fils_haut.borne_sup() > this._borne_inf) // Une borne inférieure plus interéssante à été trouvée
+			this._borne_inf = noeud.fils_haut.borne_sup();
+		*/
+		Noeud h = _rechercher_solution(noeud.fils_haut, poids_max); // On cherche une solution dans la partie haute
+		if (h.valeur() > b.valeur()) return h; // On compare la valeur haute avec le noeud, on renvoit le noeud haut si il est interessant
+		else return b; // Sinon on renvoit la solution de la partie basse
 	}
 	
 	@Override
@@ -36,7 +39,7 @@ public class PSE implements IAlgorithme
 		this._noeuds_crees = Noeud.crees;
 		
 		this._noeuds_parcourus = -1; // On ne compte pas la racine
-		this._borne_inf = 3.d;
+		this._borne_inf = 0.d;
 		Noeud solution = _rechercher_solution(racine, sac.poids_max());
 		
 		for (int i = 0; i < solution.objets.length; i++)
