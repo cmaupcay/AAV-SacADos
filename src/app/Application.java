b674aa97640fec19659c3cc5b3eb3003231cc9a5
tensaiji <47110246+tensaiji@users.noreply.github.com>
2021-10-18 21:01:33 +0200

    .txt évaluation

--- src/app/Application.java
+++ src/app/Application.java
@@ -10,8 +10,8 @@ import sac.SacADos;
 public abstract class Application 
 {
 	// Arguments par défaut
-	private static final double POIDS_MAX = 10.d;
-	private static final String FICHIER = "sujet.txt";
+	private static final double POIDS_MAX = 20.d;
+	private static final String FICHIER = "eval.txt";
 	private static final String METHODE = "pse";
 	
 	public static void main(String[] args) 
